058de35b43b9725c801a3c4bd7b487d380f7d179
Imelda Hope <imeldalarusso@gmail.com>
2024-06-26 19:45:39 +0300

    Creates arrow functions

---
+++ index.js
@@ -0,0 +1,10 @@
+//Arrow function that performs division
+const divide = (x,y) => x/y;
+divide(2000,100); 
+
+//Arrow function that squares
+const square = x => x * x;
+
+//Arrow function that performs addition
+
+const add = (x,y) => x + y;
